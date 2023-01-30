364e1ff63553606abd045d95ac79a8c4080297ad
Ivan Luk <75116559+theivanluk@users.noreply.github.com>
2023-01-30 09:30:41 -0800

    Time: 65 ms (90.72%), Space: 44.4 MB (27.54%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,23 +3,38 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    const charCount = new Map();
+//     const charCount = new Map();
     
-    for (const char of [...s]) {
-        charCount.has(char) ? 
-            charCount.set(char, charCount.get(char) + 1) : 
-            charCount.set(char, 1);
+//     for (const char of [...s]) {
+//         charCount.has(char) ? 
+//             charCount.set(char, charCount.get(char) + 1) : 
+//             charCount.set(char, 1);
+//     }
+    
+//     let palindromeLength = 0;
+//     let hasOdd = false;
+    
+//     for (const [char, count] of charCount) {
+//         count % 2 === 0 ? palindromeLength += count : 
+//             (palindromeLength += (count - 1), hasOdd = true);
+//     }
+    
+//     hasOdd ? palindromeLength += 1 : null
+    
+//     return palindromeLength;
+    const charSet = new Set();
+    let count = 0;
+    
+    for (const char of s) {
+        if(!charSet.has(char)) {
+            charSet.add(char)
+        } else {
+            charSet.delete(char);
+            count += 2;
+        }
     }
     
-    let palindromeLength = 0;
-    let hasOdd = false;
+    charSet.size ? count++ : null;
     
-    for (const [char, count] of charCount) {
-        count % 2 === 0 ? palindromeLength += count : 
-            (palindromeLength += (count - 1), hasOdd = true);
-    }
-    
-    hasOdd ? palindromeLength += 1 : null
-    
-    return palindromeLength;
+    return count;
 };
