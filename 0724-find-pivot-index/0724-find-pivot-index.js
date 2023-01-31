94cfd5b713aa6e427f43b2fcb9cd43c530180a4c
Ivan Luk <75116559+theivanluk@users.noreply.github.com>
2023-01-31 15:32:40 -0800

    Time: 79 ms (77.85%), Space: 44 MB (86.58%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -6,20 +6,17 @@ var pivotIndex = function(nums) {
     // find the rightmost sum;
     let leftSum = 0;
     let rightSum = 0;
-    let pivotIndex = 0;
     
-    for (let i = 1; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         rightSum += nums[i];
     }
     // increment through the array rightsum and add leftsum
     
-    for (let i = 1; i < nums.length; i++) {
-        if (leftSum === rightSum) return pivotIndex;
-        leftSum += nums[i - 1];
-        pivotIndex++;
+    for (let i = 0; i < nums.length; i++) {
         rightSum -= nums[i];
+        if (leftSum === rightSum) return i;
+        leftSum += nums[i]
     }
-    if (leftSum === rightSum) return pivotIndex;
     // return -1
     return -1;
 };
