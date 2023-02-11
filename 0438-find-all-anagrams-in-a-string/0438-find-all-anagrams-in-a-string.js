faadca9265fc03f791ef998f7a16cb9e4ab4b60b
Ivan Luk <75116559+theivanluk@users.noreply.github.com>
2023-02-10 16:55:42 -0800

    Time: 102 ms (80.88%), Space: 45.1 MB (83.99%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    if (s.length < p.length) return [];
+    
+    const sCount = new Array(26).fill(0);
+    const pCount = new Array(26).fill(0);
+    const result = [];
+    
+    function getCharIndex(letter) {
+        return (letter).charCodeAt(0) - ('a').charCodeAt(0);
+    }
+    
+    function isEqual(count1, count2) {
+        for (let i = 0; i < 26; i++) {
+            if (count1[i] !== count2[i]) return false;
+        }
+        return true;
+    }
+    
+    for (let i = 0; i < p.length; i++) {
+        pCount[getCharIndex(p[i])]++;
+        sCount[getCharIndex(s[i])]++;
+    }
+
+    
+    let lag = 0;
+    let lead = p.length;
+    
+    if (isEqual(sCount, pCount)) result.push(lag);
+
+    while (lead < s.length) {
+        sCount[getCharIndex(s[lag])]--;
+        sCount[getCharIndex(s[lead])]++;
+        lag++;
+        lead++;
+        if (isEqual(sCount, pCount)) result.push(lag);
+    }
+    
+    
+    return result;
+};
